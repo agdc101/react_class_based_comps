b898fac858d7da9169798ad2f3b35ea8a3b1b043
agdc101 <agdc101@gmail.com>
2023-01-12 16:36:31 +0000

    userfinder component

--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,10 @@ import classes from './User.module.css';
 import { Component } from 'react'; // adds functionality in order for class to work. - eg allows props to work!
 
 class User extends Component {
+  componentWillUnmount() {
+    console.log('runs when user is removed');
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
